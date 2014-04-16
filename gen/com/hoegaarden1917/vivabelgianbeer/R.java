417330639664a81218f76728b5cd1879ddbf41d6
Hoegaarden1917 <hoegaarden1917@gmail.com>
2014-04-16 15:21:57 +0900

    Fix small bug before release

--- gen/com/hoegaarden1917/vivabelgianbeer/R.java
+++ gen/com/hoegaarden1917/vivabelgianbeer/R.java
@@ -54,18 +54,18 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f05000b;
+        public static final int action_settings=0x7f05000c;
         public static final int answer_no=0x7f050002;
         public static final int answer_unknown=0x7f050003;
         public static final int answer_yes=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int back=0x7f050008;
-        public static final int hello_world=0x7f05000c;
+        public static final int hello_world=0x7f05000d;
         public static final int introduction=0x7f05000a;
         public static final int question_0=0x7f050004;
         public static final int question_1=0x7f050005;
         public static final int question_2=0x7f050006;
-        public static final int question_index=0x7f05000d;
+        public static final int question_index=0x7f05000b;
         public static final int start=0x7f050009;
         public static final int suggestion=0x7f050007;
     }
