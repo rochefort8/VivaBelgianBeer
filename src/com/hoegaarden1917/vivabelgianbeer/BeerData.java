8c168bac46e22d762b69d208c8f18a90eddff0b8
Yuji Ogihara <Yuji.Ogihara@jp.sony.com>
2015-02-10 16:47:18 +0900

    Add Zip extract

--- src/com/hoegaarden1917/vivabelgianbeer/BeerData.java
+++ src/com/hoegaarden1917/vivabelgianbeer/BeerData.java
@@ -1,9 +1,14 @@
 package com.hoegaarden1917.vivabelgianbeer;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import android.app.Activity;
 import android.content.Context;
@@ -15,6 +20,7 @@ public class BeerData extends Activity {
 		this.context = context ;
 		
 		FileOutputStream fileOutputStream = null ;
+		FileInputStream  in = null ;
 		String fileName = name + ".zip" ;
 		
         try {
@@ -22,12 +28,44 @@ public class BeerData extends Activity {
 			fileOutputStream.write(data) ;
 			fileOutputStream.close() ;
 			
+			extract(fileName) ;
 			archivePath = name ;
         } catch (Exception e) {
             e.printStackTrace(); 
         }
 	}
-	
+
+	public void extract(String filename) {
+	    ZipInputStream in = null;
+	    BufferedOutputStream out = null;
+	 
+	    ZipEntry zipEntry = null;
+	    int len = 0;
+	     
+	    try {
+	        in = new ZipInputStream(context.openFileInput(filename)) ;
+	    	
+	        while ((zipEntry = in.getNextEntry()) != null) {
+	            File newfile = new File(zipEntry.getName());
+	 
+	            FileOutputStream fos = context.openFileOutput(newfile.getName(),Context.MODE_PRIVATE) ;
+	            out = new BufferedOutputStream(fos) ;
+	 
+	            byte[] buffer = new byte[1024];
+	            while ((len = in.read(buffer)) != -1) {
+	                out.write(buffer, 0, len);
+	            }
+	 
+	            in.closeEntry();
+	            out.close();
+	            out = null;
+	        }
+	    } catch (FileNotFoundException e) {
+	        e.printStackTrace();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}	
 	public String getName() { return name ; }
 	public String getName_JP() { return name_JP ; }
 	public String getImagePath() { return imagePath ; }
